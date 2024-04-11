d9a94bd04ad1b432e344e3f35430f7aea9f5324e
nevy2k <birozsolt911@gmail.com>
2024-04-11 16:19:57 +0300

    I18n for HU whole app

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en, :ro]
+    config.i18n.default_locale = :hu
+    config.i18n.available_locales = [:en, :ro, :hu]
   end
 end
